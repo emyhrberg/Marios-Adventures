d2eba1956ce2ca1adfb1a6cf4dff849f4a5fccc9
erimy268 <erimy268@student.liu.se>
2023-04-28 19:27:40 +0200

    Another update pause screen, looks good now!

--- src/ui/Pause.java
+++ src/ui/Pause.java
@@ -16,78 +16,96 @@ import static constants.GameState.PLAYING;
  */
 public class Pause extends GameState {
 
-
-	// box
+	// title
 	private static final Font CUSTOM_FONT = FontLoader.loadFont("/fonts/inside-out.ttf");
-	private static final float FONT_SIZE = 48f;
-	private static final int W = (int) (265 * Game.SCALE);
-	private static final int H = (int) (220 * Game.SCALE);
-	private static final int X = Game.GAME_WIDTH / 2 - W / 2;
-	private static final int Y = Game.GAME_HEIGHT / 2 - H / 2;
-	private static final int PAD = 50;
-	private static final int ROUND = 25;
-	private static final Color BOX_COLOR = new Color(10, 10, 10, 242);
+	private static final float FONT_SIZE_BIG = 92f;
+	private static final String title = "pause menu";
+	private static final Color RED = new Color(232, 67, 33); // Color for highlighted text
+	private static final Color WHITE = new Color(224, 224, 244); // Color for default text
+	private static final Color BLACK = new Color(17, 17, 17); // Color for stroke text
+	private static final BasicStroke STROKE_SIZE = new BasicStroke(5f);
 
 	// options
-	private static final String cont = "continue";
-	private static final String quit = "save & quit";
-	private static final Color HIGHLIGHT_COLOR = new Color(232, 67, 33); // Color for highlighted text
-	private static final Color DEF_COLOR = new Color(224, 224, 244); // Color for default text
-	private static final Color STROKE_COLOR = new Color(17, 17, 17); // Color for stroke text
-	private static final BasicStroke STROKE_SIZE = new BasicStroke(4f);
+	private static final float FONT_SIZE = 56f;
+	private static final String RESUME = "resume";
+	private static final String QUIT = "save & quit";
 	private int selectedIndex = 0; // Currently selected option index
+
+	// y pos
+	private static final int TITLE_Y 	= 280;
+	private int OPTIONS_Y 				= 450;
+	private static final int PAD 		= 70;
 
     public Pause(Game game) {
 		super(game);
     }
 
 	public void drawPause(Graphics g) {
-		drawBox(g);
+		drawPauseTitle(g);
 		drawOptions(g);
 	}
 
-	private void drawBox(Graphics g) {
-		g.setColor(BOX_COLOR);
-		g.fillRoundRect(X, Y, W, H, ROUND, ROUND);
+	private void drawOptions(Graphics g) {
+		drawOption(g, RESUME, 0, 0);
+		drawOption(g, QUIT, 1, 1);
 	}
 
-	private void drawOptions(Graphics g) {
-		drawOption(g, cont, 0, 0);
-		drawOption(g, quit, 1, 1);
+	private void drawPauseTitle(Graphics g) {
+		// font and shapes
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setFont(CUSTOM_FONT.deriveFont(FONT_SIZE_BIG));
+		TextLayout tl = new TextLayout(title, g.getFont(), g2d.getFontRenderContext());
+		Shape s = tl.getOutline(null);
+
+		// position
+		FontMetrics fm = g.getFontMetrics();
+		int w = fm.stringWidth(title);
+		int x = (Game.GAME_WIDTH - w) / 2; // Centered X position
+		AffineTransform transform = AffineTransform.getTranslateInstance(x, TITLE_Y);
+		g2d.transform(transform);
+
+		// Draw black stroke
+		g2d.setStroke(STROKE_SIZE);
+		g2d.setColor(BLACK);
+		g2d.draw(s);
+
+		// draw white fill
+		g2d.setColor(WHITE);
+		g2d.fill(s);
+
+		// Restore the original transform
+		g2d.setTransform(new AffineTransform());
 	}
 
 	private void drawOption(Graphics g, String option, int index, int yIndex) {
 		// font and shapes
 		Graphics2D g2d = (Graphics2D) g;
-		g2d.setFont(CUSTOM_FONT);
-		g.setFont(g.getFont().deriveFont(FONT_SIZE));
+		g.setFont(CUSTOM_FONT.deriveFont(FONT_SIZE));
 		TextLayout tl = new TextLayout(option, g.getFont(), g2d.getFontRenderContext());
 		Shape s = tl.getOutline(null);
 
 		// position
 		FontMetrics fm = g.getFontMetrics();
 		int w = fm.stringWidth(option);
-		int h = fm.getHeight();
-		int x = X + (W - w) / 2;
-		int y = Y + (H - h) / 2 + fm.getAscent() - PAD; // continue Y
-		if (yIndex == 0) {
-			y = Y + (H - h) / 2 + fm.getAscent() - PAD; // continue Y
+		int x = (Game.GAME_WIDTH - w) / 2; // Centered X position
+		if (yIndex == 0) { // first
+			OPTIONS_Y -= PAD;
 		} else if (yIndex == 1) {
-			y = Y + (H - h) / 2 + fm.getAscent() + PAD; // quit Y
+			OPTIONS_Y += PAD; // second
 		}
-		AffineTransform transform = AffineTransform.getTranslateInstance(x, y);
+		AffineTransform transform = AffineTransform.getTranslateInstance(x, OPTIONS_Y);
 		g2d.transform(transform);
 
 		// Draw black stroke
 		g2d.setStroke(STROKE_SIZE);
-		g2d.setColor(STROKE_COLOR);
+		g2d.setColor(BLACK);
 		g2d.draw(s);
 
 		// Draw white or highlighted color
 		if (index == selectedIndex) {
-			g2d.setColor(HIGHLIGHT_COLOR);
+			g2d.setColor(RED);
 		} else {
-			g2d.setColor(DEF_COLOR);
+			g2d.setColor(WHITE);
 		}
 		g2d.fill(s);
 
@@ -98,12 +116,13 @@ public class Pause extends GameState {
 	public void keyPressed(KeyEvent e) {
 		switch (e.getKeyCode()) {
 			// Scrolling infinite!
-			case KeyEvent.VK_W -> selectedIndex = (selectedIndex + 1) % 2;
-			case KeyEvent.VK_S -> selectedIndex = (selectedIndex - 1 + 2) % 2;
+			case KeyEvent.VK_W, KeyEvent.VK_UP -> selectedIndex = (selectedIndex + 1) % 2;
+			case KeyEvent.VK_S, KeyEvent.VK_DOWN -> selectedIndex = (selectedIndex - 1 + 2) % 2;
 
+			// Only up and down scroll allowed!
 //			case KeyEvent.VK_W -> selectedIndex = Math.max(selectedIndex - 1, 0);
 //			case KeyEvent.VK_S -> selectedIndex = Math.min(selectedIndex + 1, 1);
-			case KeyEvent.VK_ENTER -> handleSelection();
+			case KeyEvent.VK_ENTER, KeyEvent.VK_SPACE -> handleSelection();
 			case KeyEvent.VK_ESCAPE -> game.getPlaying().resetGameSavePoint();
 		}
 	}
