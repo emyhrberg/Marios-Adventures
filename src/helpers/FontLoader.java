a9fa2f3e27d6ade27edec205e544a7b18cef6481
erimy268 <erimy268@student.liu.se>
2023-05-09 09:12:57 +0200

    Fixed jar file

--- src/helpers/FontLoader.java
+++ src/helpers/FontLoader.java
@@ -1,28 +1,28 @@
-package helpers;
-
-import java.awt.*;
-import java.io.IOException;
-import java.io.InputStream;
-
-/**
- * This class loads a custom font
- */
-public class FontLoader {
-
-    public static Font loadFont(String fontName) {
-        try (InputStream is = FontLoader.class.getResourceAsStream(fontName)) {
-
-            // No font resource found, return standard
-            if (is == null)
-                return new Font("Arial", Font.PLAIN, 30);
-
-            // Create and register the custom font
-            Font font = Font.createFont(Font.TRUETYPE_FONT, is).deriveFont(32f);
-            GraphicsEnvironment.getLocalGraphicsEnvironment().registerFont(font);
-            return font;
-        } catch (FontFormatException | IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-}
+//package helpers;
+//
+//import java.awt.*;
+//import java.io.IOException;
+//import java.io.InputStream;
+//
+///**
+// * This class loads a custom font
+// */
+//public class FontLoader {
+//
+//    public static Font loadFont(String fontName) {
+//        try (InputStream is = FontLoader.class.getResourceAsStream(fontName)) {
+//
+//            // No font resource found, return standard
+//            if (is == null)
+//                return new Font("Arial", Font.PLAIN, 30);
+//
+//            // Create and register the custom font
+//            Font font = Font.createFont(Font.TRUETYPE_FONT, is).deriveFont(32f);
+//            GraphicsEnvironment.getLocalGraphicsEnvironment().registerFont(font);
+//            return font;
+//        } catch (FontFormatException | IOException e) {
+//            e.printStackTrace();
+//            return null;
+//        }
+//    }
+//}
